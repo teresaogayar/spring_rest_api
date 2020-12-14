d6b99f5db04d038335764ce00c969fb92643040e
Teresa Ogáyar De Dios <tod00001@red.ujaen.es>
2020-12-14 12:33:31 +0100

    spring lecture -v2

--- src/main/java/com/example/spring_rest_api/controller/MainController.java
+++ src/main/java/com/example/spring_rest_api/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.example.spring_rest_api.controller;
+import com.example.spring_rest_api.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 // CheetSheet
 // ALT + Enter -> auto-hints
@@ -13,6 +17,14 @@ public class MainController {
     @GetMapping("/")        // http://www.localhost:8080/
     public String homepage(){
         return "Hello in hompage";
+    }
+    @GetMapping("/user/id={userId}&name={name}&lastName={lastName}&email={email}&password={password}")
+    public User getUser(
+            @PathVariable("userId") int userId, @PathVariable("name") String name,
+            @PathVariable("lastName") String lastName, @PathVariable("email") String email,
+            @PathVariable("password") String password
+    ){
+        return new User(userId, name, lastName, email,password, LocalDateTime.now(), false);
     }
 
 }
