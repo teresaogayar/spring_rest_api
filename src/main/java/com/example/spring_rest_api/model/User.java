dea6a662ef8c8911db3cfdac7dc38501340a29d4
Teresa Ogáyar De Dios <tod00001@red.ujaen.es>
2020-12-14 13:42:19 +0100

    lecture 3

--- src/main/java/com/example/spring_rest_api/model/User.java
+++ src/main/java/com/example/spring_rest_api/model/User.java
@@ -5,14 +5,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 // model class determine the data structure in our program
+// and map the user class on database entity
+@Entity     // map the user class on database entity - support ORM
 @Getter     // automatic injection code for this class covers getters
 @Setter     // automatic injection code for this class covers setters
 @AllArgsConstructor // automatic generation of contructor with all fields in arguments
 @NoArgsConstructor  // automatic generation of contructor without arguments
 public class User {
+    @Id                                                 // PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENTATION
     private int userId;
     private String name;
     private String lastName;
